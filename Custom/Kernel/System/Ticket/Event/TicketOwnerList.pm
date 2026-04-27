86f9f658640018aade546c8003ec37df3cd4ff73
vadikonline1 <vadikonline1@gmail.com>
2026-04-27 14:56:45 +0300

    Refactor TicketOwnerList for improved readability

--- Custom/Kernel/System/Ticket/Event/TicketOwnerList.pm
+++ Custom/Kernel/System/Ticket/Event/TicketOwnerList.pm
@@ -3,8 +3,6 @@ package Kernel::System::Ticket::Event::TicketOwnerList;
 use strict;
 use warnings;
 
-use Kernel::System::VariableCheck qw(:all);
-
 our @ObjectDependencies = (
     'Kernel::System::Log',
     'Kernel::System::Ticket',
@@ -14,9 +12,7 @@ our @ObjectDependencies = (
 
 sub new {
     my ( $Type, %Param ) = @_;
-    my $Self = {};
-    bless( $Self, $Type );
-    return $Self;
+    return bless {}, $Type;
 }
 
 sub Run {
@@ -24,55 +20,25 @@ sub Run {
 
     my $LogObject = $Kernel::OM->Get('Kernel::System::Log');
 
-    # validate params
     for my $Needed (qw(Data Event Config UserID)) {
-        if ( !$Param{$Needed} ) {
-            $LogObject->Log(
-                Priority => 'error',
-                Message  => "Need $Needed!",
-            );
-            return;
-        }
+        return if !$Param{$Needed};
     }
 
     return 1 if $Param{Event} ne 'TicketOwnerUpdate';
 
-    my $TicketID = $Param{Data}->{TicketID};
-    if ( !$TicketID ) {
-        $LogObject->Log(
-            Priority => 'error',
-            Message  => "Need TicketID!",
-        );
-        return;
-    }
+    my $TicketID = $Param{Data}->{TicketID} || return;
 
-    my $DynamicFieldName = $Param{Config}->{DynamicField};
-    if ( !$DynamicFieldName ) {
-        $LogObject->Log(
-            Priority => 'error',
-            Message  => "DynamicField not defined!",
-        );
-        return;
-    }
+    my $DynamicFieldName = $Param{Config}->{DynamicField} || return;
 
     my $DynamicFieldObject        = $Kernel::OM->Get('Kernel::System::DynamicField');
     my $DynamicFieldBackendObject = $Kernel::OM->Get('Kernel::System::DynamicField::Backend');
 
     my $DynamicField = $DynamicFieldObject->DynamicFieldGet(
         Name => $DynamicFieldName,
-    );
+    ) || return;
 
-    if (
-        !$DynamicField
-        || $DynamicField->{ObjectType} ne 'Ticket'
-        || $DynamicField->{FieldType} ne 'Text'
-    ) {
-        $LogObject->Log(
-            Priority => 'error',
-            Message  => "Invalid DynamicField configuration!",
-        );
-        return;
-    }
+    return if $DynamicField->{ObjectType} ne 'Ticket';
+    return if $DynamicField->{FieldType} ne 'Text';
 
     my $TicketObject = $Kernel::OM->Get('Kernel::System::Ticket');
 
@@ -86,7 +52,6 @@ sub Run {
         push @OwnerArray, $Owner->{UserFullname};
     }
 
-    # remove duplicates but keep order
     my %Seen;
     @OwnerArray = grep { !$Seen{$_}++ } @OwnerArray;
 
@@ -97,11 +62,6 @@ sub Run {
         ObjectID           => $TicketID,
         Value              => $OwnerStrg,
         UserID             => $Param{UserID},
-    );
-
-    $LogObject->Log(
-        Priority => 'notice',
-        Message  => "Updated InvolvedOwner for Ticket $TicketID",
     );
 
     return 1;
